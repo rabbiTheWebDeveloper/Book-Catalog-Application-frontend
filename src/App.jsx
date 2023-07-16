7a7d3a2241b5874c89249216f3e2cbe46b67abd1
S M AL RABBI <rabbithedeveloper@gmail.com>
2023-07-16 16:23:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import "./App.css";
+import Banner from "./component/Banner";
+import BestBook from "./component/BestBook";
+import Brand from "./component/Brand";
+import Footer from "./component/Footer";
+import Gallery from "./component/Gallery";
+import Header from "./component/header";
+
+function App() {
+ 
+
+  return (
+    <>
+      <Header></Header>
+      <Banner></Banner>
+      <Brand></Brand>
+
+      
+      <BestBook></BestBook>
+
+      <Gallery></Gallery>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
